bb239b37269e2d1d815d0aff3b0bfabb5ec4009a
tamararoos <tamara.roos@stud.hslu.ch>
2025-12-12 15:35:16 +0100

    anpassungen java script

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-/*document.querySelectorAll('.scroll-container').forEach(container => {
-  // Originalinhalt duplizieren
-  const content = container.innerHTML;
-  container.innerHTML += content + content; // dreifach für Sicherheit
-
-  // Startposition in der Mitte setzen
-  const scrollHeight = container.scrollHeight / 3;
-  container.scrollTop = scrollHeight;
-
-  container.addEventListener('scroll', () => {
-    const maxScroll = container.scrollHeight / 3 * 2;
-    const minScroll = container.scrollHeight / 3;
-
-    // Wenn zu weit nach unten gescrollt -> zurück in die Mitte
-    if (container.scrollTop >= maxScroll) {
-      container.scrollTop -= scrollHeight;
-    }
-
-    // Wenn zu weit nach oben gescrollt -> zurück in die Mitte
-    if (container.scrollTop <= 0) {
-      container.scrollTop += scrollHeight;
-    }
-  });
-});*/
 
 document.querySelectorAll('.scroll-container').forEach((item) => {
   // Duplicate content 50x to allow seamless scrolling
