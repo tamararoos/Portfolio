412c95aa30d46e7ec0b698045815c7055a7038a7
tamararoos <tamara.roos@stud.hslu.ch>
2026-01-16 16:35:05 +0100

    gallery

--- main.js
+++ main.js
@@ -1,48 +1,46 @@
-
-document.querySelectorAll('.scroll-container').forEach((item) => {
-  const originalContent = item.innerHTML
-  let duplicatedContent = ''
+document.querySelectorAll(".scroll-container").forEach((item) => {
+  const originalContent = item.innerHTML;
+  let duplicatedContent = "";
   for (let i = 0; i < 3; i++) {
-    duplicatedContent += originalContent
+    duplicatedContent += originalContent;
   }
-  item.innerHTML = duplicatedContent
+  item.innerHTML = duplicatedContent;
 
   // Scroll to center at the beginning
-  item.scrollTop = (item.scrollHeight - item.clientHeight) / 2
-  
-  item.addEventListener('scroll', () => {
-    const topEdge = item.scrollHeight * .1
-    const bottomEdge = item.scrollHeight * .9
-    const itemsHeight = item.scrollHeight / 3
+  item.scrollTop = (item.scrollHeight - item.clientHeight) / 2;
+
+  item.addEventListener("scroll", () => {
+    const topEdge = item.scrollHeight * 0.1;
+    const bottomEdge = item.scrollHeight * 0.9;
+    const itemsHeight = item.scrollHeight / 3;
 
     if (item.scrollTop < topEdge) {
-      item.scrollTop += itemsHeight
+      item.scrollTop += itemsHeight;
     } else if (item.scrollTop + item.clientHeight > bottomEdge) {
-      item.scrollTop -= itemsHeight
+      item.scrollTop -= itemsHeight;
     }
-  })
+  });
 
   // Automatically scroll slowly downwards except when hovered. Use requestAnimationFrame for smoother scrolling.
-  let isHovered = false
+  let isHovered = false;
 
-  item.addEventListener('mouseenter', () => {
-    isHovered = true
-  })
+  item.addEventListener("mouseenter", () => {
+    isHovered = true;
+  });
 
-  item.addEventListener('mouseleave', () => {
-    isHovered = false
-  })
+  item.addEventListener("mouseleave", () => {
+    isHovered = false;
+  });
 
   // Scroll speed
-  let scrollSpeed = (Math.random() -0.5)*5
-
+  let scrollSpeed = (Math.random() - 0.5) * 5;
 
   function scrollStep() {
     if (!isHovered) {
-      item.scrollTop += scrollSpeed
+      item.scrollTop += scrollSpeed;
     }
-    requestAnimationFrame(scrollStep)
+    requestAnimationFrame(scrollStep);
   }
 
-  requestAnimationFrame(scrollStep)
-})
+  requestAnimationFrame(scrollStep);
+});
